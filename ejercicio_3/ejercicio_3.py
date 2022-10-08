342d09de16de6da75edac5213837e80150323136
JorgeM28 <jorge2224764@correo.uis.edu.co>
2022-10-07 20:02:10 -0500

    EJERCICIO 3

---
+++ ejercicio_3/ejercicio_3.py
@@ -0,0 +1,14 @@
+print("-----------------------")
+print("-------FIBONACCI-------")
+print("-----------------------")
+
+a = 0
+b = 1
+c = a + b
+num = 0
+while c < 1000:
+    num = num + 1
+    print("El numero",c,"es el",num,"en a serie fibonacci")
+    a = b
+    b = c
+    c = a + b
